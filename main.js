c4109e1765628a52f776c9f65b1b30402989c486
Admin <reanajb@gmail.com>
2024-02-27 12:21:56 -0500

    html & css updates, beginning process on jQuery/main.js

--- main.js
+++ main.js
@@ -1,4 +1,37 @@
 console.log('functionality')
 
-const moneyValue = $('div')
+const moneyValue = $('.content')
 console.log(moneyValue)
+
+// const questionDisplay = $('.question-display')
+
+
+
+
+//jeopardy = array with objects in it
+//once click on grid square, lloks at $ amount on square & feed you random questions with that value
+//math.random -->generate number of first index of array to the end of length
+//search for random one, matches keep and display if not keep searching
+  //target innerText -->give number in HTML
+
+
+moneyValue.on('click', (event)=>{
+    console.log(event.target.innerText)
+    //when reading innerText value, matches value in obj display question
+    
+    if(event.target.innerText === jeopardy.value){
+        $('.question-display').innerHTML(jeopardy.Math.random().question)
+    }
+    // questionDisplay.innerText(`${jeopardyCategories.question}`)
+})
+
+
+
+
+
+
+$('button').on('click', (event)=>{
+    console.log(event.target.innerText)
+    const text = $('input')
+    // text.val(`${jeopardyCategories.answer}`)
+})
