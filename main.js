7f17d35479915a9660f76cdfb18dff5234eae9d5
Admin <reanajb@gmail.com>
2024-02-28 12:21:02 -0500

    js work: randomize question & display in HTML

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-console.log('functionality')
+// console.log(jeopardy)
 
-const moneyValue = $('.content')
-console.log(moneyValue)
+// console.log(jeopardy[0].question)
+// console.log(jeopardy[1].question)
 
-// const questionDisplay = $('.question-display')
+
+const moneyScore = $('#score')
+const categoryOptions = $('.content')
+const questionDisplay = $('.question-display')
+
+const answerBox = $('#user-answer')
+const submitButton = $('#submit-button')
+
+let score = 0
 
 
 
@@ -15,23 +23,41 @@ console.log(moneyValue)
   //target innerText -->give number in HTML
 
 
-moneyValue.on('click', (event)=>{
+
+
+categoryOptions.on('click', (event)=>{
     console.log(event.target.innerText)
-    //when reading innerText value, matches value in obj display question
-    
-    if(event.target.innerText === jeopardy.value){
-        $('.question-display').innerHTML(jeopardy.Math.random().question)
+    //when reading innerText value, matches value in obj --> display question
+    let randomizeIndex = Math.floor(Math.random() * Object.values(jeopardy).length)
+    let questionValue = jeopardy['value']
+    if(event.value === questionValue){
+        questionDisplay.html(jeopardy[randomizeIndex].question)
+
     }
-    // questionDisplay.innerText(`${jeopardyCategories.question}`)
-})
-
-
-
-
-
-
+   
+    //work out logic for text box
+  //have click listener on submit inside of this case
+   //if this is correct question, sumbit button and text boxes work
+   //when its clicked do check --> if it is correct add to total 
+   //if not subtract score from total
 $('button').on('click', (event)=>{
     console.log(event.target.innerText)
     const text = $('input')
     // text.val(`${jeopardyCategories.answer}`)
 })
+   
+  
+ 
+   
+  
+   
+   
+})
+   
+
+
+
+
+
+
+
